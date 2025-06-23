af938939b280cb5773275f187b8f5d42620e96a7
Daniel Tuladhar <dtuladhar@wpi.edu>
2025-06-23 14:51:14 +0545

    commit 5: - line graph page -- multiselect for stock and field -- moving average - rsi graph -- date average period -- multiselect for stock -- overbought and oversold adjusters

--- Stock_project/stock/urls.py
+++ Stock_project/stock/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.stock_dashboard, name='stock_dashboard'),
     path('test-influxdb/', views.test_influxdb_view, name='test_influxdb'),
+    path('dashboard/candlestick/', views.stock_dashboard, name='candlestick_dashboard'),
+    path('dashboard/line/', views.line_dashboard, name='line_dashboard'),
+    path('dashboard/rsi/', views.rsi_dashboard, name='rsi_dashboard'),
 ] 
